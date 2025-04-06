be3c62d43853490e61d605df4eb496098770c8d0
Biswajitkara2004 <biswajit.papul2004@gmail.com>
2025-04-06 17:42:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,33 +15,29 @@ if Choice == "linux":
 elif Choice == "windows":
     
 
-    setconfigfor = input("Choose configuration type: \n1. Hosting\n2. Development\n")
+    setconfigfor = input("Choose configuration type: \n1. Desktop\n2. Enviroment\n3. Server\n4. software\n5. optimization system")
     if setconfigfor == "1":
-        print("You chose Hosting configuration.")
+        print("You chose Desktop configuration.")
         # Add hosting configuration code here
         
     elif setconfigfor == "2":
-        print("You chose Development configuration.")
+        print("You chose Enviroment configuration.")
         idechoice = input("do you need python idle or other Ide's (y/n)")
         if idechoice == 'y':
             #add ide configuration code here
-            ide = input("Choose your IDE: \n1. VSCode\n2. PyCharm\n3. IntelliJ\n4. Eclipse\n5. Android Studio\n")
+            ide = input("Choose your IDE: \n1. VSCode\n2. PyCharm\n3. IntelliJ\n")
             if ide == "1":
                 print("You chose VSCode.")
             elif ide == "2":
                 print("You chose PyCharm.")
             elif ide == "3":
                 print("You chose IntelliJ.")
-            elif ide == "4":
-                print("You chose Eclipse.")
-            elif ide == "5":
-                print("You chose Android Studio.")
             else:
                 print("Invalid choice. Please choose a valid IDE.")
         else:
         
             # Add development configuration code here
-            devenv = input("Choose your development environment: \n1. Python\n2. C/C++\n3. Web development\n4. Java development\n5. Go\n6. Rust\n7. Shell scripting\n8. SQL\n9. Android\n")
+            devenv = input("Choose your development environment: \n1. Python\n2. C/C++\n3. Web development\n4. Java development\n")
             if devenv == "1":
                 
                 # Use the PyConfig module to fetch Python versions
